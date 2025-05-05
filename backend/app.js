0dea5238e3cb1fd4272a6546186985258366679f
ebisa <ebisatesfaye30@gmail.com>
2025-05-05 23:25:43 +0300

    port added

--- backend/app.js
+++ backend/app.js
@@ -181,7 +181,7 @@ app.post('/login', async (req, res) => {
   
 
 
-const PORT = 5000;
+const PORT =  process.env.PORT || 5000;
 // set up the listener
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
